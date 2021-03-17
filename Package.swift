d8c1cf8baac61ab08b28cbc4b42a74a6c5b0e812
Pablo Viciano <pablo.viciano@gmail.com>
2021-03-17 08:03:46 +0100

    commit

--- Package.swift
+++ Package.swift
@@ -4,21 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "BFramework",
+    name: "TestBFramework",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "BFramework",
-            targets: ["BFramework"]),
+            name: "TestBFramework",
+            targets: ["TestBFramework"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "AFramework", path: "A.xcframework")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "BFramework", path: "B.xcframework")
+        .binaryTarget(name: "TestBFramework", path: "TestBFramework.xcframework")
     ]
 )
